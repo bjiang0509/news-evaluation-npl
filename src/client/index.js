e69a2a7bfc735d7d68ce62ca69f43a5805902192
bjiang0509 <86031227+bjiang0509@users.noreply.github.com>
2022-02-23 18:08:16 -0800

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { checkUrl } from "./js/urlCheck.js";
 import { handleSubmit, postData, updateUI } from "./js/formHandler.js";
 
 import "./styles/index.scss";
+console.log("hello")
 
 export {
     checkUrl,
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
@@ -40,6 +41,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
