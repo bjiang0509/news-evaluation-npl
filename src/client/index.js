80fa4c2b00c73d4715c117ca457434000ffd0f5b
bjiang0509 <86031227+bjiang0509@users.noreply.github.com>
2022-02-23 17:19:34 -0800

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,6 @@ import { checkUrl } from "./js/urlCheck.js";
 import { handleSubmit, postData, updateUI } from "./js/formHandler.js";
 
 import "./styles/index.scss";
-console.log("hello")
 
 export {
     checkUrl,
