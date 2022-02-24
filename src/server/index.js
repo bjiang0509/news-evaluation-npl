8baf04940b6c8da84aa5a9f0b929abb76103f591
bjiang0509 <86031227+bjiang0509@users.noreply.github.com>
2022-02-23 16:16:35 -0800

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv'); //allow us to use env variables
 dotenv.config()
-console.log(process.env.API_KEY)
 
 let projectData = {}
 const apiKey = process.env.API_KEY;
@@ -26,7 +25,9 @@ app.get('/', function (req, res) {
 const handleFetchApi = async(req, res) => {
     //req.body.apiUrl
     //req.body.url
+    //let key = '0220ca408ea683689233a27434395f06'
     const url = `${req.body.apiUrl}key=${apiKey}&url=${req.body.url}&lang=en`;
+    //console.log(req.body.apiUrl+", "+key+ ", "+ req.body.url)
     const response = await fetch(url)
     try{
         const data = await response.json();
